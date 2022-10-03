824cf8d27c9488a11294aabb4a9079ecad60a1db
Yongbin <yongbin97@yonsei.ac.kr>
2022-10-03 22:46:31 +0900

    feat: Create Post

--- app/models/post.py
+++ app/models/post.py
@@ -5,13 +5,20 @@ from pydantic import BaseModel
 
 @dataclass
 class Post(BaseModel):
+    id: int
     writer_id: int
     content: str
     like_num: int
+
+
+class CreatePost(BaseModel):
+    content: str
+    like_num: int = 0
+    writer_id: int
 
 
 @dataclass
 class Comment(BaseModel):
     content: str
     post_id: int
-    user_id : int
+    user_id: int
